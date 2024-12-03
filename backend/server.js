35f9709a92e02446bf478a1a2e220cad4229dca4
LeonidVolosov <leonid.volosov@student.karazin.ua>
2024-12-03 13:52:17 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,22 +7,17 @@ const mqttClient = mqtt.connect('mqtt://localhost');
 
 mqttClient.on('connect', () => {
     console.log('Connected to MQTT broker');
+    mqttClient.subscribe('device/data');
     mqttClient.subscribe('device/control');
-    mqttClient.subscribe('device/getData');
 });
 
 mqttClient.on('message', (topic, message) => {
-    if (topic === 'device/control') {
-        if (message.toString() === 'on') {
-            console.log('Turning on the device');
-        } else if (message.toString() === 'off') {
-            console.log('Turning off the device');
-        }
+    if (topic === 'device/data') {
+        console.log('Received data:', message.toString());
     }
 
-    if (topic === 'device/getData') {
-        const data = "Temperature: 22°C";
-        mqttClient.publish('device/data', data);
+    if (topic === 'device/control') {
+        console.log('Received control command:', message.toString());
     }
 });
 
@@ -31,6 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
+
 
