c557f463d5ccd489a931f83b8aa8e4f63b385021
LeonidVolosov <leonid.volosov@student.karazin.ua>
2024-12-03 14:47:26 +0100

    Update this.js

--- frontend/this.js
+++ frontend/this.js
@@ -1,20 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MQTT Device Control</title>
-</head>
-<body>
-    <h1>MQTT Device Control</h1>
+const client = mqtt.connect('ws://localhost:9001');  
 
-    <button id="onButton">Turn On</button>
-    <button id="offButton">Turn Off</button>
-    <button id="getDataButton">Get Data</button>
+client.on('connect', function () {
+    console.log('Connected to MQTT broker');
+    client.subscribe('device/data');  
+});
 
-    <p id="dataDisplay"></p>
+document.getElementById('onButton').addEventListener('click', function () {
+    client.publish('device/control', 'on');  
+});
 
-    <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
-    <script src="this.js"></script> 
-</body>
-</html>
+document.getElementById('offButton').addEventListener('click', function () {
+    client.publish('device/control', 'off');  
+});
+
+document.getElementById('getDataButton').addEventListener('click', function () {
+    client.publish('device/getData', 'request');  
+});
+
+
+client.on('message', function (topic, message) {
+    if (topic === 'device/data') {
+        document.getElementById('dataDisplay').innerText = 'Data: ' + message.toString();  
+    }
+});
+
