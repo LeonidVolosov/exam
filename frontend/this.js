d25aa4f27d3acca266713dd9e774021d37cd1c57
LeonidVolosov <leonid.volosov@student.karazin.ua>
2024-12-03 14:43:02 +0100

    Update and rename index.html to this.js

---
+++ frontend/this.js
@@ -0,0 +1,23 @@
+const client = mqtt.connect('ws://localhost:9001'); 
+
+client.on('connect', function () {
+    console.log('Connected to MQTT broker');
+});
+
+document.getElementById('onButton').addEventListener('click', function () {
+    client.publish('device/control', 'on');
+});
+
+document.getElementById('offButton').addEventListener('click', function () {
+    client.publish('device/control', 'off');
+});
+
+document.getElementById('getDataButton').addEventListener('click', function () {
+    client.publish('device/getData', 'request');
+});
+
+client.on('message', function (topic, message) {
+    if (topic === 'device/data') {
+        document.getElementById('dataDisplay').innerText = 'Data: ' + message.toString();
+    }
+});
