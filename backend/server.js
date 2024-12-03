710e1d8db124fa004bb17738d7ac8a4a4563d70d
LeonidVolosov <leonid.volosov@student.karazin.ua>
2024-12-03 14:38:52 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
-const mqttClient = mqtt.connect('mqtt://localhost');
+const mqttClient = mqtt.connect('ws://localhost:9001');  
 
 mqttClient.on('connect', () => {
     console.log('Connected to MQTT broker');
-    mqttClient.subscribe('device/data');  
-    mqttClient.subscribe('device/control');  
+    mqttClient.subscribe('device/data');
+    mqttClient.subscribe('device/control');
 });
 
 mqttClient.on('message', (topic, message) => {
-    if (topic === 'device/data') {
-        console.log('Received data:', message.toString());
+    if (topic === 'device/control') {
+        if (message.toString() === 'on') {
+            console.log('Turning on the device');
+        } else if (message.toString() === 'off') {
+            console.log('Turning off the device');
+        }
     }
 
-    if (topic === 'device/control') {
-        console.log('Received control command:', message.toString());
+    if (topic === 'device/getData') {
+        const data = "Temperature: 22°C"; 
+        mqttClient.publish('device/data', data);
     }
 });
 
 app.get('/', (req, res) => {
     res.send('IoT Backend');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
 
 app.listen(port, () => {
